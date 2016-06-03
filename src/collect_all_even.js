c82c68a6adcad31eebb4a6096db72b3b9cdf7124
CuiFg <1174910857@qq.com>
2016-06-03 17:15:09 +0800

    Update collect_all_even.js

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,7 +1,18 @@
 'use strict';
 
-function collect_all_even(collection) {
-  //在这里写入代码
+function collect_all_even(collection)
+{
+
+	var shuZu=[];
+	for(var i=0; i<newarr.length; i++)
+	{
+		if(newarr[i]%2==0)
+		{
+			shuZu.push(newarr[i]);
+		}
+	}
+	return shuZu;
+}
 }
 
 module.exports = collect_all_even;
